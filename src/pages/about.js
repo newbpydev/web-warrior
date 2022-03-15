0c85f8466d0f1beac5fb73aaee8d4d46b53907a7
newbpydev <newbpydev@gmail.com>
2022-03-15 13:23:36 -0400

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,30 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+          dolorum esse cupiditate mollitia ut dignissimos veritatis similique,
+          et suscipit, consequuntur distinctio saepe quis voluptates blanditiis
+          doloremque incidunt recusandae, alias vero.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+          dolorum esse cupiditate mollitia ut dignissimos veritatis similique,
+          et suscipit, consequuntur distinctio saepe quis voluptates blanditiis
+          doloremque incidunt recusandae, alias vero.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+          dolorum esse cupiditate mollitia ut dignissimos veritatis similique,
+          et suscipit, consequuntur distinctio saepe quis voluptates blanditiis
+          doloremque incidunt recusandae, alias vero.
+        </p>
+      </div>
+    </Layout>
+  )
+}
